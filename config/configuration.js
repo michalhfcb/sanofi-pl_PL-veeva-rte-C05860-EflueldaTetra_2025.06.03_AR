514959f9b79b2cda2fdbd6d5781c22e76a11f41f
michalhfcb <michal.henning@fcbhealthspark.com>
2025-06-04 13:21:50 +0200

    cfg changed

--- config/configuration.js
+++ config/configuration.js
@@ -2,7 +2,7 @@ export const config = {
 	RTE_NAME: 'C05860_EflueldaTetra_24proc_mniej_przypadków_grypy_os_starsze_HQ',
 	RTE_ALT: 'sanofi',
 	RTE_PREHEADER: '',
-	RTE_TYPE: 'VEEVA', //POSSIBLE VALUES: VEEVA, SFMC, PITCHER, MASSMAILING, WP, INIS
+	RTE_TYPE: 'SFMC', //POSSIBLE VALUES: VEEVA, SFMC, PITCHER, MASSMAILING, WP, INIS
 	RTE_SANOFI: 'YES', // POSSIBLE VALUES: YES, NO - Controls whether to include survey links
  	RTE_DIVISION:'', // ONLY FOR SFMC SANOFI, POSSIBLE VALUES: GENMED, VACCINES, SPECIALITY_CARE
 	RTE_UNSUBSCRIBE_MAIL: 'mailto:noreply@example.com', //ONLY FOR MASSMAILING
